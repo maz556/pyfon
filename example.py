a85ff2a12d0a6541c9d6877905802d2ded3506d2
maz556 <maz80008@gmail.com>
2022-07-11 19:21:10 -0500

    Add variables, loops, and input notebooks

---
+++ example.py
@@ -0,0 +1,18 @@
+# int # 1, 2, 3, 4, 5, -4, 0, 69
+# str # "hello", "why?", "what the fuck", "69", " ", ""
+# float # 1.5, 4.56, 
+# bool # True, False
+# None
+
+# 0   0   0   1   0   1   0   1  =  21
+# 128 64  32  16  8   4   2   1
+
+# 10000010 = 130
+
+# print(print(6 + 5) is None)
+
+print("Hello world!")
+print("Hello world!")
+print("Hello world!")
+print("Hello world!")
+
